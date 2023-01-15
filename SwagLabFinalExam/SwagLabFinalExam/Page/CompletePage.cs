b45cfb3260ba7e0ca9930992f54ef21b34afe61c
Ana Vuco <anavuco24@gmail.com>
2023-01-15 20:42:46 +0100

    Changes

---
+++ SwagLabFinalExam/SwagLabFinalExam/Page/CompletePage.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using SwagLabFinalExam.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagLabFinalExam.Page
+{
+    public class CompletePage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+        
+        public IWebElement OrderFinished => driver.FindElement(By.CssSelector("#checkout_complete_container .complete-header"));
+        public IWebElement ButtonBurger => driver.FindElement(By.Id("react-burger-menu-btn"));
+        public IWebElement Logout => driver.FindElement(By.Id("logout_sidebar_link"));
+    }
+}
---
+++ SwagLabFinalExam/SwagLabFinalExam/Page/YourInfoPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+using SwagLabFinalExam.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwagLabFinalExam.Page
+{
+    public class YourInfoPage
+    {
+        private IWebDriver driver = WebDrivers.Instance;
+        
+        public IWebElement FirstName => driver.FindElement(By.Id("first-name"));
+        public IWebElement LastName => driver.FindElement(By.Id("last-name"));
+        public IWebElement ZipCode => driver.FindElement(By.Id("postal-code"));
+        public IWebElement ButtonContinue => driver.FindElement(By.Id("continue"));
+    }
+}
